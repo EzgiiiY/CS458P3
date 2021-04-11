a3f3fa000e97bf6d6d6fb7c0d8b66340a2ba94c4
ezgi <ezqi401@hotmail.com>
2021-04-11 15:51:31 +0300

    added + - to format

--- project3/src/CityLocator.js
+++ project3/src/CityLocator.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Button,Input } from 'antd';
+import { Card, Button, Input } from 'antd';
 import 'antd/dist/antd.css';
 import '../src/App.css';
 import Header from './Header';
@@ -14,94 +14,93 @@ Geocode.setApiKey("AIzaSyAHlJQ6EdfWT_UVPNpagMe7sS9kz1N-diU");
 // set response language. Defaults to english.
 Geocode.setLanguage("en");
 Geocode.setLocationType("ROOFTOP");
-const re = new RegExp("^([0-9]+([.][0-9]*)?|[.][0-9]+)$");
+const re = new RegExp("^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$");
 
 
 class CityLocator extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            latitude:0,
-            longitude:0,
+        this.state = {
+            latitude: 0,
+            longitude: 0,
             city: '',
-            loading:false,
-            validLat:true,
-            validLong:true,
-        }; 
-        this.locateCity=this.locateCity.bind(this);
+            loading: false,
+            validLat: true,
+            validLong: true,
+        };
+        this.locateCity = this.locateCity.bind(this);
         this.onChangeLat = this.onChangeLat.bind(this);
         this.onChangeLong = this.onChangeLong.bind(this);
 
     }
 
-    async locateCity(){
-        this.setState({loading:true})
+    async locateCity() {
+        this.setState({ loading: true })
         console.log("Locate city called.");
         await Geocode.fromLatLng(this.state.latitude.toString(), this.state.longitude.toString()).then(
             (response) => {
                 let city;
-              const address = response.results[0].address_components;
-              for(let i = 0; i< address.length;i++)
-                {
-                    if(address[i].types.includes("administrative_area_level_1"))
-                        city=address[i].long_name;
+                const address = response.results[0].address_components;
+                for (let i = 0; i < address.length; i++) {
+                    if (address[i].types.includes("administrative_area_level_1"))
+                        city = address[i].long_name;
                 }
-              console.log("address: " + address+ " city: " + city );
-              this.setState({city:city, loading:false})
+                console.log("address: " + address + " city: " + city);
+                this.setState({ city: city, loading: false })
             },
             (error) => {
-              console.error(error);
+                console.error(error);
             }
         );
-       console.log(this.state)
-    }
-
-    onChangeLat(e){
-        console.log(e.target.value)
-        if(re.test(e.target.value))
-            this.setState({validLat:true, latitude:e.target.value})
-        else
-            this.setState({validLat:false})
         console.log(this.state)
     }
 
-    onChangeLong(e){
+    onChangeLat(e) {
         console.log(e.target.value)
-        if(re.test(e.target.value))
-            this.setState({validLong:true, longitude:e.target.value})
+        if (re.test(e.target.value))
+            this.setState({ validLat: true, latitude: e.target.value })
         else
-            this.setState({validLong:false})
+            this.setState({ validLat: false })
         console.log(this.state)
     }
 
-    render(){
+    onChangeLong(e) {
+        console.log(e.target.value)
+        if (re.test(e.target.value))
+            this.setState({ validLong: true, longitude: e.target.value })
+        else
+            this.setState({ validLong: false })
+        console.log(this.state)
+    }
+
+    render() {
         return <div>
-            
+
             <p>Please enter coordinates to see nearest city</p>
             <input
                 className="latitude"
                 type="text"
-                name="latitude" 
+                name="latitude"
                 placeholder="Latitude"
-                style={{ backgroundColor: "#fff",borderRadius:"4px" ,borderColor:"#333", color:"#8c8c8c",marginRight: ".5rem"}}
+                style={{ backgroundColor: "#fff", borderRadius: "4px", borderColor: "#333", color: "#8c8c8c", marginRight: ".5rem" }}
                 onChange={this.onChangeLat}
             >
 
             </input>
-            {!this.state.validLat && <p className="error-lat" style= {{color:"orange", fontWeight:"bold"}}>*hello</p>}
+            {!this.state.validLat && <p className="error-lat" style={{ color: "orange", fontWeight: "bold" }}>*hello</p>}
             <input
                 className="longitude"
                 type="text"
-                name="longitude" 
+                name="longitude"
                 placeholder="Longitude"
-                style={{ backgroundColor: "#fff",borderRadius:"4px" ,borderColor:"#333", color:"#8c8c8c",marginRight: ".5rem"}}
+                style={{ backgroundColor: "#fff", borderRadius: "4px", borderColor: "#333", color: "#8c8c8c", marginRight: ".5rem" }}
                 onChange={this.onChangeLong}
             >
-            
+
             </input>
             {!this.state.validLong && <p className="error-long">hello</p>}
             <CityLocatorButton className="locate-city" buttonValue="locate" buttonAction={this.locateCity}>
-                
+
             </CityLocatorButton>
             <p className="result-text">{`This location is in ${this.state.city}`}</p>
         </div>
