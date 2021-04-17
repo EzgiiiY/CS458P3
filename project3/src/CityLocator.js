a12c42639d0ff26bb021f7a0db1337681ae7b1d7
ezgi <ezqi401@hotmail.com>
2021-04-17 17:28:15 +0300

    city locator refactored

--- project3/src/CityLocator.js
+++ project3/src/CityLocator.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-import { Card, Button, Spin } from 'antd';
+import { Spin } from 'antd';
 
 import 'antd/dist/antd.css';
 import '../src/App.css';
-import Header from './Header';
-
-
 import CityLocatorButton from "./CityLocatorButton";
 import Geocode from "react-geocode";
 
@@ -16,8 +13,9 @@ Geocode.setApiKey("AIzaSyAHlJQ6EdfWT_UVPNpagMe7sS9kz1N-diU");
 Geocode.setLanguage("en");
 Geocode.setLocationType("ROOFTOP");
 const re = new RegExp("^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$");
-
-
+const style1 = { color: "orange", fontWeight: "bold" }
+const style2 = { backgroundColor: "#fff", borderRadius: "4px", borderColor: "#333", color: "#8c8c8c", marginRight: ".5rem" }
+const style3 = { color: "blue", fontWeight: "bold", fontSize: "25px" }
 class CityLocator extends Component {
     constructor(props) {
         super(props);
@@ -37,7 +35,6 @@ class CityLocator extends Component {
 
     async locateCity() {
         this.setState({ loading: true })
-        console.log("Locate city called.");
         await Geocode.fromLatLng(this.state.latitude.toString(), this.state.longitude.toString()).then(
             (response) => {
                 let city;
@@ -46,32 +43,27 @@ class CityLocator extends Component {
                     if (address[i].types.includes("administrative_area_level_1"))
                         city = address[i].long_name;
                 }
-                console.log("address: " + address + " city: " + city);
                 this.setState({ city: city, loading: false })
             },
             (error) => {
                 console.error(error);
+                this.setState({ city: "not found", loading: false })
             }
         );
-        console.log(this.state)
     }
 
     onChangeLat(e) {
-        console.log(e.target.value)
         if (re.test(e.target.value))
             this.setState({ validLat: true, latitude: e.target.value })
         else
             this.setState({ validLat: false })
-        console.log(this.state)
     }
 
     onChangeLong(e) {
-        console.log(e.target.value)
         if (re.test(e.target.value))
             this.setState({ validLong: true, longitude: e.target.value })
         else
             this.setState({ validLong: false })
-        console.log(this.state)
     }
 
     render() {
@@ -83,30 +75,31 @@ class CityLocator extends Component {
                     type="text"
                     name="latitude"
                     placeholder="Latitude"
-                    style={{ backgroundColor: "#fff", borderRadius: "4px", borderColor: "#333", color: "#8c8c8c", marginRight: ".5rem" }}
+                    style={style2}
                     onChange={this.onChangeLat}
-                >
-
-                </input>
-                {!this.state.validLat && <p className="error-lat" style={{ color: "orange", fontWeight: "bold" }}>*hello</p>}
+                />
+                {!this.state.validLat &&
+                    <p className="error-lat" style={style1}>Invalid Latitude Input</p>}
                 <input
                     className="longitude"
                     type="text"
                     name="longitude"
                     placeholder="Longitude"
-                    style={{ backgroundColor: "#fff", borderRadius: "4px", borderColor: "#333", color: "#8c8c8c", marginRight: ".5rem" }}
+                    style={style2}
                     onChange={this.onChangeLong}
-                >
+                />
+                {!this.state.validLong &&
+                    <p className="error-long" style={style1}>Invalid Longitude Input</p>}
+                <CityLocatorButton 
+                    className="locate-city" 
+                    buttonValue="locate" 
+                    buttonAction={this.locateCity}
+                />
 
-                </input>
-                {!this.state.validLong && <p className="error-long" style={{ color: "orange", fontWeight: "bold" }}>hello</p>}
-                <CityLocatorButton className="locate-city" buttonValue="locate" buttonAction={this.locateCity}>
-
-                </CityLocatorButton>
                 <br></br>
-                {this.state.loading && <Spin style={{marginTop:"2%"}}size="large"></Spin>}
+                {this.state.loading && <Spin style={{ marginTop: "2%" }} size="large"></Spin>}
                 {this.state.city &&
-                    <p className="result-text-geoc" style={{ color: "blue", fontWeight: "bold", fontSize: "25px" }} >{`This location is in ${this.state.city}`}</p>
+                    <p className="result-text-geoc" style={style3} >{`This location is in ${this.state.city}`}</p>
                 }
             </div>);
     }
